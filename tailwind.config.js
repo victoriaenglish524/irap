dfbf2cede16e4cfa3028bd552c8977ba2d95e5d2
victoriaenglish524 <victoriaenglish565@gmail.com>
2025-04-08 19:37:00 -0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  theme: {
-    extend: {},
-  },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: { extend: {} },
   plugins: [],
-}
+};
